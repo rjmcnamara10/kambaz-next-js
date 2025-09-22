1356aa9fce54bc8637615f1dbe2a3d1576c41be1
rjmcnamara10 <rjmcnamara10@gmail.com>
2025-09-22 17:29:45 -0400

    completed 2.1 through 2.3

---
+++ app/Labs/Lab2/Float.tsx
@@ -0,0 +1,102 @@
+export default function Float() {
+  return (
+    <div id="wd-float-divs">
+      <h2>Float</h2>
+      <div>
+        <img
+          className="wd-float-right"
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+        />
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in sem
+        dapibus, varius neque ut, feugiat turpis. Sed quam risus, consectetur ac
+        sollicitudin vel, vestibulum nec purus. Suspendisse rutrum, lorem id
+        egestas fermentum, sapien lectus vulputate quam, sit amet semper turpis
+        ex et diam. Morbi est metus, bibendum vel cursus nec, tincidunt nec
+        eros. Nam pulvinar turpis vitae odio finibus feugiat. Quisque id purus
+        at nisl facilisis fermentum. Nam varius tellus tellus, in dignissim nisi
+        sollicitudin a. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Morbi in sem dapibus, varius neque ut, feugiat turpis. Sed quam risus,
+        consectetur ac sollicitudin vel, vestibulum nec purus. Suspendisse
+        rutrum, lorem id egestas fermentum, sapien lectus vulputate quam, sit
+        amet semper turpis ex et diam. Morbi est metus, bibendum vel cursus nec,
+        tincidunt nec eros. Nam pulvinar turpis vitae odio finibus feugiat.
+        Quisque id purus at nisl facilisis fermentum. Nam varius tellus tellus,
+        in dignissim nisi sollicitudin a.
+        <img
+          className="wd-float-left"
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+        />
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in sem
+        dapibus, varius neque ut, feugiat turpis. Sed quam risus, consectetur ac
+        sollicitudin vel, vestibulum nec purus. Suspendisse rutrum, lorem id
+        egestas fermentum, sapien lectus vulputate quam, sit amet semper turpis
+        ex et diam. Morbi est metus, bibendum vel cursus nec, tincidunt nec
+        eros. Nam pulvinar turpis vitae odio finibus feugiat. Quisque id purus
+        at nisl facilisis fermentum. Nam varius tellus tellus, in dignissim nisi
+        sollicitudin a. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Morbi in sem dapibus, varius neque ut, feugiat turpis. Sed quam risus,
+        consectetur ac sollicitudin vel, vestibulum nec purus. Suspendisse
+        rutrum, lorem id egestas fermentum, sapien lectus vulputate quam, sit
+        amet semper turpis ex et diam. Morbi est metus, bibendum vel cursus nec,
+        tincidunt nec eros. Nam pulvinar turpis vitae odio finibus feugiat.
+        Quisque id purus at nisl facilisis fermentum. Nam varius tellus tellus,
+        in dignissim nisi sollicitudin a.
+        <img
+          className="wd-float-right"
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+        />
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in sem
+        dapibus, varius neque ut, feugiat turpis. Sed quam risus, consectetur ac
+        sollicitudin vel, vestibulum nec purus. Suspendisse rutrum, lorem id
+        egestas fermentum, sapien lectus vulputate quam, sit amet semper turpis
+        ex et diam. Morbi est metus, bibendum vel cursus nec, tincidunt nec
+        eros. Nam pulvinar turpis vitae odio finibus feugiat. Quisque id purus
+        at nisl facilisis fermentum. Nam varius tellus tellus, in dignissim nisi
+        sollicitudin a. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Morbi in sem dapibus, varius neque ut, feugiat turpis. Sed quam risus,
+        consectetur ac sollicitudin vel, vestibulum nec purus. Suspendisse
+        rutrum, lorem id egestas fermentum, sapien lectus vulputate quam, sit
+        amet semper turpis ex et diam. Morbi est metus, bibendum vel cursus nec,
+        tincidunt nec eros. Nam pulvinar turpis vitae odio finibus feugiat.
+        Quisque id purus at nisl facilisis fermentum. Nam varius tellus tellus,
+        in dignissim nisi sollicitudin a.
+        <img
+          className="wd-float-left"
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+        />
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in sem
+        dapibus, varius neque ut, feugiat turpis. Sed quam risus, consectetur ac
+        sollicitudin vel, vestibulum nec purus. Suspendisse rutrum, lorem id
+        egestas fermentum, sapien lectus vulputate quam, sit amet semper turpis
+        ex et diam. Morbi est metus, bibendum vel cursus nec, tincidunt nec
+        eros. Nam pulvinar turpis vitae odio finibus feugiat. Quisque id purus
+        at nisl facilisis fermentum. Nam varius tellus tellus, in dignissim nisi
+        sollicitudin a. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Morbi in sem dapibus, varius neque ut, feugiat turpis. Sed quam risus,
+        consectetur ac sollicitudin vel, vestibulum nec purus. Suspendisse
+        rutrum, lorem id egestas fermentum, sapien lectus vulputate quam, sit
+        amet semper turpis ex et diam. Morbi est metus, bibendum vel cursus nec,
+        tincidunt nec eros. Nam pulvinar turpis vitae odio finibus feugiat.
+        Quisque id purus at nisl facilisis fermentum. Nam varius tellus tellus,
+        in dignissim nisi sollicitudin a.
+        <div className="wd-float-done"></div>
+      </div>
+      <div>
+        <div className="wd-float-left wd-dimension-portrait wd-bg-color-yellow">
+          Yellow
+        </div>
+        <div className="wd-float-left wd-dimension-portrait wd-bg-color-blue wd-fg-color-white">
+          Blue
+        </div>
+        <div className="wd-float-left wd-dimension-portrait wd-bg-color-red">
+          Red
+        </div>
+        <img
+          className="wd-float-right"
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+        />
+        <div className="wd-float-done"></div>
+      </div>
+    </div>
+  );
+}
