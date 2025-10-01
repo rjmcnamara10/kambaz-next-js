85a3cf311fcb4f78be9c7b765867b52b34211944
rjmcnamara10 <rjmcnamara10@gmail.com>
2025-10-01 13:29:22 -0400

    fixing Kambaz Navigation sidebar

--- app/(Kambaz)/Navigation.tsx
+++ app/(Kambaz)/Navigation.tsx
@@ -1,23 +1,11 @@
-"use client";
 import { AiOutlineDashboard } from "react-icons/ai";
 import { IoCalendarOutline } from "react-icons/io5";
 import { LiaBookSolid, LiaCogSolid } from "react-icons/lia";
 import { FaInbox, FaRegCircleUser } from "react-icons/fa6";
 import { ListGroup, ListGroupItem } from "react-bootstrap";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 
 export default function KambazNavigation() {
-  const pathname = usePathname();
-
-  const getLinkClass = (path: string) => {
-    return pathname.startsWith(path) ? "text-danger" : "text-white";
-  };
-
-  const getItemClass = (path: string) => {
-    return pathname.startsWith(path) ? "bg-white" : "bg-black";
-  };
-
   return (
     <ListGroup
       className="rounded-0 position-fixed bottom-0 top-0 d-none d-md-block bg-black z-2"
@@ -44,65 +32,55 @@ export default function KambazNavigation() {
           Account
         </Link>
       </ListGroupItem>
-      <ListGroupItem
-        className={`border-0 text-center ${getItemClass("/Dashboard")}`}
-      >
+      <ListGroupItem className="border-0 text-center bg-white">
         <Link
           href="/Dashboard"
           id="wd-dashboard-link"
-          className={`text-decoration-none ${getLinkClass("/Dashboard")}`}
+          className="text-decoration-none text-danger"
         >
           <AiOutlineDashboard className="fs-1 text-danger" />
           <br />
           Dashboard
         </Link>
       </ListGroupItem>
-      <ListGroupItem
-        className={`border-0 text-center ${getItemClass("/Courses")}`}
-      >
+      <ListGroupItem className="border-0 text-center bg-black">
         <Link
           href="/Dashboard"
           id="wd-course-link"
-          className={`text-decoration-none ${getLinkClass("/Courses")}`}
+          className="text-decoration-none text-white"
         >
           <LiaBookSolid className="fs-1 text-danger" />
           <br />
           Courses
         </Link>
       </ListGroupItem>
-      <ListGroupItem
-        className={`border-0 text-center ${getItemClass("/Calendar")}`}
-      >
+      <ListGroupItem className="border-0 text-center bg-black">
         <Link
           href="/Calendar"
           id="wd-calendar-link"
-          className={`text-decoration-none ${getLinkClass("/Calendar")}`}
+          className="text-decoration-none text-white"
         >
           <IoCalendarOutline className="fs-1 text-danger" />
           <br />
           Calendar
         </Link>
       </ListGroupItem>
-      <ListGroupItem
-        className={`border-0 text-center ${getItemClass("/Inbox")}`}
-      >
+      <ListGroupItem className="border-0 text-center bg-black">
         <Link
           href="/Inbox"
           id="wd-inbox-link"
-          className={`text-decoration-none ${getLinkClass("/Inbox")}`}
+          className="text-decoration-none text-white"
         >
           <FaInbox className="fs-1 text-danger" />
           <br />
           Inbox
         </Link>
       </ListGroupItem>
-      <ListGroupItem
-        className={`border-0 text-center ${getItemClass("/Labs")}`}
-      >
+      <ListGroupItem className="border-0 text-center bg-black">
         <Link
           href="/Labs"
           id="wd-labs-link"
-          className={`text-decoration-none ${getLinkClass("/Labs")}`}
+          className="text-decoration-none text-white"
         >
           <LiaCogSolid className="fs-1 text-danger" />
           <br />
