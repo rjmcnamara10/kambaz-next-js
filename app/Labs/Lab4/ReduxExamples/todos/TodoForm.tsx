86d3b0dcf9a1616a246f8fd163299acf7a7fc9cb
rjmcnamara10 <rjmcnamara10@gmail.com>
2025-10-27 15:48:33 -0400

    completed lab 4

---
+++ app/Labs/Lab4/ReduxExamples/todos/TodoForm.tsx
@@ -0,0 +1,27 @@
+import { FormControl, Button, ListGroupItem } from "react-bootstrap";
+import { useSelector, useDispatch } from "react-redux";
+import { addTodo, updateTodo, setTodo } from "./todosReducer";
+
+export default function TodoForm() {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const { todo } = useSelector((state: any) => state.todosReducer);
+  const dispatch = useDispatch();
+
+  return (
+    <ListGroupItem>
+      <Button onClick={() => dispatch(addTodo(todo))} id="wd-add-todo-click">
+        Add
+      </Button>
+      <Button
+        onClick={() => dispatch(updateTodo(todo))}
+        id="wd-update-todo-click"
+      >
+        Update
+      </Button>
+      <FormControl
+        value={todo.title}
+        onChange={(e) => dispatch(setTodo({ ...todo, title: e.target.value }))}
+      />
+    </ListGroupItem>
+  );
+}
--- app/Labs/page.tsx
+++ app/Labs/page.tsx
@@ -21,6 +21,11 @@ export default function Labs() {
             Lab 3: JavaScript Fundamentals
           </Link>
         </li>
+        <li>
+          <Link href="/Labs/Lab4" id="wd-lab4-link">
+            Lab 4: State Management
+          </Link>
+        </li>
       </ul>
     </div>
   );
