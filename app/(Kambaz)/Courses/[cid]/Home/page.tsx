c149138a6ddb342aa6bad8bf3b83aafd2a011fa0
rjmcnamara10 <rjmcnamara10@gmail.com>
2025-11-11 15:56:23 -0500

    bug fix

--- app/(Kambaz)/Courses/[cid]/Home/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Home/page.tsx
@@ -1,28 +1,8 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 import Modules from "../Modules/page";
 import CourseStatus from "./Status";
-import { useEffect, useRef } from "react";
-import { useSelector } from "react-redux";
-import { useParams, redirect } from "next/navigation";
 
 export default function Home() {
-  const { cid } = useParams();
-  const { currentUser } = useSelector((state: any) => state.accountReducer);
-  const { enrollments } = useSelector((state: any) => state.enrollmentsReducer);
-  const blocked = useRef(false);
-
-  useEffect(() => {
-    const enrolled = enrollments.some(
-      (e: any) => e.user === currentUser?._id && e.course === cid
-    );
-    if ((!currentUser || !enrolled) && !blocked.current) {
-      blocked.current = true;
-      alert("You are not enrolled in this course.");
-      redirect("/Dashboard");
-    }
-  }, [currentUser, enrollments, cid]);
-
   return (
     <div id="wd-home">
       <div className="d-flex" id="wd-home">
